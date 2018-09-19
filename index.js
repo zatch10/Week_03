d4c953eaf9d60986e543bb8649b151ce4ba886b2
arshiam2 <arshiam2@illinois.edu>
2018-09-19 16:33:31 -0500

    loops, json

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-# Week_03
 
-# Javascript Fundamentals
-
-### For Loops
-
-For loops will start at a value and iterate through a range. 
-
-```Javascript 
 let introCourses = [125, 173, 225, 233, 241, 374]
 for (let i = 0; i < introCourses.length; i++) { 
   console.log(introCourses[i])
 }
-```
 
-### While Loops
-
-While looks will continue till the condition is false. 
-
-```Javascript 
 let count = 1;
 while (count < 10){
     console.log("Count " + count)
     count += 1;
 }
 console.log("Done counting")
-```
 
-### JSON
-
-JSON is a way of storing different types of information in an object in javascript. 
-
-```Javascript
 let student = {
   name: "Aria",
   age: 20, 
@@ -45,22 +25,14 @@ let student = {
 
 console.log(student.name)
 console.log(student.friends[0].name)
-```
 
-
-## What does this do?
-
-```Javascript
-
-let student = {
-  courses = []
+let students = {
+  courses : []
 }
-let introCourses = [125, 173, 225, 233, 241, 374]
 
 for (let i = 0; i < introCourses.length; i++) { 
-    student.courses.push(introCourses[i])
+    students.courses.push(introCourses[i])
 }
-console.log(student)
-```
+console.log(students)
 
 
